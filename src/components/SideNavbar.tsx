f788fc63bc2f6208509e72b725d6812e2cdf7e74
Flook1 <139752743+Flook1@users.noreply.github.com>
2024-05-29 06:16:38 +0700

    react error

---
+++ src/components/SideNavbar.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState } from "react";
+import { Nav } from "./ui/nav";
+import { Button } from "./ui/button";
+
+import { useWindowWidth } from "@react-hook/window-size";
+
+type Props = {};
+
+import {
+  UsersRound,
+  LayoutDashboard,
+  ShoppingCart,
+  Settings,
+  ChevronRight,
+} from "lucide-react";
+
+export default function SideNavbar({}: Props) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const onlyWidth = useWindowWidth();
+  const mobileWidth = onlyWidth < 768;
+
+  function toggleSidebar() {
+    setIsCollapsed(!isCollapsed);
+  }
+
+  return (
+    <div className="relative min-w-[px] bg-yellow-300 px-3 pb-10 pt-24">
+      {!mobileWidth && (
+        <div className="absolute right-[-20px] top-7">
+          <Button
+            onClick={toggleSidebar}
+            variant="secondary"
+            className="rounded-full p-2"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
+      <Nav
+        isCollapsed={mobileWidth ? true : isCollapsed}
+        links={[
+          {
+            title: "Dashboard",
+            href: "/",
+            icon: LayoutDashboard,
+            variant: "default",
+          },
+          {
+            title: "Users",
+            href: "/users",
+            icon: UsersRound,
+            variant: "ghost",
+          },
+          {
+            title: "Orders",
+            href: "/orders",
+            icon: ShoppingCart,
+            variant: "ghost",
+          },
+          {
+            title: "Settings",
+            href: "/settings",
+            icon: Settings,
+            variant: "ghost",
+          },
+        ]}
+      />
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,37 @@
-import { GeistSans } from "geist/font/sans";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+import { cn } from "~/lib/utils";
+import { Inter as FontSans } from "next/font/google";
+import SideNavbar from "~/components/SideNavbar";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <main className={GeistSans.className}>
-        <Component {...pageProps} />
+    <body
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable,
+      )}
+    >
+      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#ffa65d] to-[#dac029]">
+        <SessionProvider session={session}>
+          <div>
+            <SideNavbar />
+          </div>
+          <Component {...pageProps} />
+        </SessionProvider>
       </main>
-    </SessionProvider>
+    </body>
   );
 };
 
